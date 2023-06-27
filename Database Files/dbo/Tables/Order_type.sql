9a9eba1b6006ce692e2129372b43ce57a0a2d82d
Sohaib Salman <salmansohaib2001@gmail.com>
2023-06-27 16:39:15 +0500

    first commit

---
+++ Database Files/dbo/Tables/Order_type.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[Order_type] (
+    [id]   NCHAR (1)     NULL,
+    [type] NVARCHAR (50) NULL
+);
+
+
+GO
+
---
+++ Database Files/dbo/Tables/Product.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[Product] (
+    [id]             NCHAR (2)     NULL,
+    [product_title]  NVARCHAR (50) NULL,
+    [price_per_unit] MONEY         NULL
+);
+
+
+GO
+
